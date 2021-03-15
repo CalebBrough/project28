dc93347dcd11dab4d98038476df005fd184d4c25
CalebBrough <76225424+CalebBrough@users.noreply.github.com>
2021-03-15 09:24:23 -0600

    Update Stone.js

--- js/Stone.js
+++ js/Stone.js
@@ -7,7 +7,7 @@ class Stone {
         }
         this.body = Bodies.circle(x, y, radius, options);
         this.radius = radius;
-        this.image = loadImage("images/stone.png");
+        this.image = loadImage("stone.png");
 
         World.add(world, this.body);
     }
